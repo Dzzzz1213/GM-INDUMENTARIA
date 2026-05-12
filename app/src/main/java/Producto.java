b2793876269e1f6ea1cb27f2e72d15547b387c53
Dzzzz1213 <160103794+Dzzzz1213@users.noreply.github.com>
2026-05-12 14:28:32 -0300

    constructor productos

---
+++ app/src/main/java/Producto.java
@@ -0,0 +1,10 @@
+package com.example.gmindumentaria;
+public class Producto {
+String nombre;
+int precio;
+public Producto(String nombre, int precio){
+    this.nombre = nombre;
+    this.precio = precio;
+}
+}
+
